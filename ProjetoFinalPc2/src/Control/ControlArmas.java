5b636f1096fa3ce390fa737f27a7b476b147da0e
gedeaopereira <gedeao40@gmail.com>
2021-04-14 23:33:54 -0300

    teste

--- ProjetoFinalPc2/src/Control/ControlArmas.java
+++ ProjetoFinalPc2/src/Control/ControlArmas.java
@@ -1,8 +1,10 @@
 package Control;
 
 import View.ViewArmas;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ControlArmas {
+public class ControlArmas implements ActionListener {
 
     private ViewArmas viewArmas;
 
@@ -10,7 +12,40 @@ public class ControlArmas {
         this.viewArmas = new ViewArmas();
         this.viewArmas.setVisible(true);
         viewArmas.setLocationRelativeTo(null);
-        
+        viewArmas.getBtn_adicionarAcessorio().addActionListener(this);
+        viewArmas.getBtn_buscar().addActionListener(this);
+        viewArmas.getBtn_excluirAcessorio().addActionListener(this);
+        viewArmas.getBtn_novo().addActionListener(this);
+        viewArmas.getBtn_excluirArma().addActionListener(this);
+        viewArmas.getBtn_salvar().addActionListener(this);
+        viewArmas.getBtn_voltar().addActionListener(this);
+
     }
-    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if ("adicionarAcessorio".equals(e.getActionCommand())) {
+            ControlTraje controlTraje = new ControlTraje();
+            this.viewArmas.dispose();
+        } else if ("buscar".equals(e.getActionCommand())) {
+            ControlArmas controlArmas = new ControlArmas();
+            this.viewArmas.dispose();
+        } else if ("excluirAcessorio".equals(e.getActionCommand())) {
+            ControlArmas controlArmas = new ControlArmas();
+            this.viewArmas.dispose();
+        } else if ("novo".equals(e.getActionCommand())) {
+            ControlArmas controlArmas = new ControlArmas();
+            this.viewArmas.dispose();
+        } else if ("excluirArma".equals(e.getActionCommand())) {
+            ControlArmas controlArmas = new ControlArmas();
+            this.viewArmas.dispose();
+        } else if ("salvar".equals(e.getActionCommand())) {
+            viewArmas.dispose();
+            System.exit(0);
+        } else if ("voltar".equals(e.getActionCommand())) {
+            viewArmas.dispose();
+            System.exit(0);
+        }
+    }
+
 }
