727932183387c3c479086428d525891c28973f4d
gedeaopereira <gedeao40@gmail.com>
2021-04-15 01:05:21 -0300

    asd

--- ProjetoFinalPc2/src/Control/ControlArmas.java
+++ ProjetoFinalPc2/src/Control/ControlArmas.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
 
@@ -40,28 +42,78 @@ public class ControlArmas implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if ("adicionarAcessorio".equals(e.getActionCommand())) {
-
+            listAcessorios.addElement(viewArmas.getTxt_equipamentoArma().getText());
         } else if ("buscar".equals(e.getActionCommand())) {
-            ControlArmas controlArmas = new ControlArmas();
-            this.viewArmas.dispose();
+
+            ArmasDAO armasDAO = new ArmasDAO();
+            try {
+                for (Armas arma : armasDAO.ler(this.viewArmas.getTxt_buscar().getText())) {
+                    listArmas.addElement(arma.getNome());
+                }
+            } catch (IOException ex) {
+                System.out.println("Erro ao caregar arquivo");
+            }
+
         } else if ("excluirAcessorio".equals(e.getActionCommand())) {
-            ControlArmas controlArmas = new ControlArmas();
+
             this.viewArmas.dispose();
         } else if ("novo".equals(e.getActionCommand())) {
+
             this.viewArmas.getTxt_equipamentoArma().setText("");
             this.viewArmas.getTxt_nomeArma().setText("");
+            this.viewArmas.getBtn_salvar().setActionCommand("cadastrar");
+            this.viewArmas.getBtn_salvar().setText("Cadastrar");
             this.listAcessorios.clear();
+
         } else if ("excluirArma".equals(e.getActionCommand())) {
+
             ArmasDAO armasDAO = new ArmasDAO();
             Armas armas = new Armas(this.viewArmas.getList_armas().getName());
             armasDAO.deleta(armas);
             listArmas.remove(this.viewArmas.getList_armas().getSelectedIndex());
-        } else if ("salvar".equals(e.getActionCommand())) {
-            viewArmas.dispose();
-            System.exit(0);
+
+        } else if ("cadastrar".equals(e.getActionCommand())) {
+            ArmasDAO armasDAO = new ArmasDAO();
+            ArrayList<String> acessorios = new ArrayList<>();
+            if (listAcessorios.getSize() > 0) {
+                for (int i = 0; i < listAcessorios.getSize(); i++) {
+                    acessorios.add(listAcessorios.get(i).toString());
+                }
+                Armas armas = new Armas(this.viewArmas.getTxt_nomeArma().getText(), acessorios);
+                armasDAO.cadastra(armas);
+            } else {
+                Armas armas = new Armas(this.viewArmas.getTxt_nomeArma().getText());
+                armasDAO.cadastra(armas);
+            }
+            listArmas.clear();;
+            try {
+                for (Armas arma : armasDAO.ler()) {
+                    listArmas.addElement(arma.getNome());
+                }
+            } catch (IOException ex) {
+                System.out.println("Erro ao caregar arquivo");
+            }
+
+        } else if ("salva".equals(e.getActionCommand())) {
+            ArmasDAO armasDAO = new ArmasDAO();
+            ArrayList<String> acessorios = new ArrayList<>();
+            if (listAcessorios.getSize() > 0) {
+                for (int i = 0; i < listAcessorios.getSize(); i++) {
+                    acessorios.add(listAcessorios.get(i).toString());
+                }
+                Armas armas = new Armas(this.viewArmas.getTxt_nomeArma().getText(), acessorios);
+                armasDAO.atualiza(armas);
+            } else {
+                Armas armas = new Armas(this.viewArmas.getTxt_nomeArma().getText());
+                armasDAO.atualiza(armas);
+            }
+            listArmas.clear();;
+
         } else if ("voltar".equals(e.getActionCommand())) {
-            viewArmas.dispose();
-            System.exit(0);
+
+            ControlInicio controlInicio = new ControlInicio();
+            this.viewArmas.dispose();
+
         }
     }
 
--- ProjetoFinalPc2/src/Model/Armas.java
+++ ProjetoFinalPc2/src/Model/Armas.java
@@ -17,19 +17,6 @@ public class Armas {
         this.nome = nome;
     }
 
-    public void adicionarAcessorios(String nome) {
-        acessorios.add(nome);
-    }
-
-    public void removerAcessorios(String nome) {
-        for (int i = 0; i < acessorios.size(); i++) {
-            if(acessorios.get(i).equals(nome)){
-                acessorios.remove(i);
-                break;
-            }
-        }
-    }
-
     public String getNome() {
         return nome;
     }
